556aa431918211db943b4dbf9c345791fea370a1
Audrow Nash <audrow@hey.com>
2023-05-06 11:44:08 -0500

    Add testing and watering algorithm

---
+++ src/controller/watering-algorithm.test.ts
@@ -0,0 +1,41 @@
+import { daysUntilWatering, getDaysBetweenDates } from "./watering-algorithm"
+
+describe("daysUntilWatering", () => {
+  it("returns 7", () => {
+    expect(daysUntilWatering()).toEqual(7)
+  })
+})
+
+describe("getDaysBetweenDates", () => {
+  it.each([
+    {
+      dateStrings: [],
+      expected: [],
+    },
+    {
+      dateStrings: ["2021-01-01"],
+      expected: [],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-01"],
+      expected: [0],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02"],
+      expected: [1],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02", "2021-01-03"],
+      expected: [1, 1],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02", "2021-01-03", "2021-01-06"],
+      expected: [1, 1, 3],
+    },
+  ])("%j", ({ dateStrings, expected }) => {
+    const dates = dateStrings.map(
+      (dateString) => new Date(Date.parse(dateString))
+    )
+    expect(getDaysBetweenDates(...dates)).toEqual(expected)
+  })
+})
